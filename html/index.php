26623bab0394372ad9ddb26de1142374c61be651
estellechoi <estele.choi@gmail.com>
2019-05-13 14:55:55 +0900

    first commit for helloworld.

---
+++ html/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+</head>
+
+<body>
+
+<img src="https://cdn-images-1.medium.com/max/1600/1*KBwrbjppSZILvsiNVyoQKw.jpeg"
+width=700 height=200>
+<img src="https://cdn-images-1.medium.com/max/1600/1*dv1Uv84_ZUx-BmQgDOXWDw.png"
+   width=200> <p>
+
+ <table cellspacing=0 cellpadding=10 border=1 align=left>
+
+ <tr align=center>
+ <td><a href="page1.html">page1</a></td>
+ </tr>
+ <tr align=center>
+ <td><a href="test.html">test</a></td>
+ </tr>
+ <tr align=center>
+ <td><a href="test.html">test</a></td>
+ </tr>
+ <tr align=center>
+ <td><a href="test.html">test</a></td>
+ </tr>
+ <tr align=center>
+ <td><a href="test.html">test</a></td>
+ </tr>
+ <tr align=center>
+ <td><a href="test.html">test</a></td>
+ </tr>
+
+ </table>
+
+</body>
+
+</html>
